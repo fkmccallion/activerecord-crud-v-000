27d11ff2815b88dfa496be964c72df1124f3bcc7
Frank McCallion <fkmccallion@gmail.com>
2018-04-01 00:27:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  movie = Movie.find_by(id: 1)
+  movie = Movie.first
   movie.title
 end
 
